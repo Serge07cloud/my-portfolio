3144832c8974f242303c7e4808f843c16e90a713
Serge07cloud <wisdomandy.5@gmail.com>
2025-11-05 21:52:58 +0000

    Change featured projects font color

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,8 @@ const Projects = () =>
 {
   return (
     <>
-      <div>My Featured Projects</div>
+      <div className="text-center pb-44">
+        <h1 className="text-4xl font-semibold tracking-tight text-balance text-white sm:text-4xl">My Featured <span className="font-bold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-blue-500">Projects</span></h1></div>
     </>
   )
 };
