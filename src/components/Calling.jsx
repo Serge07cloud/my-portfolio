7ddb51b6f0a89bf84a78ca7b19d07f73345b07e3
Serge07cloud <wisdomandy.5@gmail.com>
2025-11-08 18:36:05 +0000

    Add Calling component and update Main to include it

---
+++ src/components/Calling.jsx
@@ -0,0 +1,16 @@
+const Calling = () =>
+{
+  return (
+    <>
+      <div className="text-center mx-auto max-w-6xl pb-48 sm:pb-48 lg:pb-48">
+        <h1 className="text-5xl font-bold tracking-tight text-balance text-white sm:text-7xl">Ready to build <span className="bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-blue-500">something Amazing</span>
+        </h1>
+        <div className="text-lg font-medium py-5 text-pretty max-w-4xl mx-auto text-gray-400 sm:text-xl/8">
+          Let's collaborate to bring your vision to life. From concept to deployment, I deliver high-quality software solutions that exceed expectations and drive real business results.
+        </div>
+      </div>
+    </>
+  )
+};
+
+export default Calling
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,6 +4,9 @@ const Welcome = () =>
     <>
       <div>
         <div className="mx-auto max-w-2xl py-48 sm:py-48 lg:py-48">
+          <div aria-hidden="true" className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
+            <div style={ { clipPath: 'polygon(74.1 % 44.1 %, 100 % 61.6 %, 97.5 % 26.9 %, 85.5 % 0.1 %, 80.7 % 2 %, 72.5 % 32.5 %, 60.2 % 62.4 %, 52.4 % 68.1 %, 47.5 % 58.3 %, 45.2 % 34.5 %, 27.5 % 76.7 %, 0.1 % 64.9 %, 17.9 % 100 %, 27.6 % 76.8 %, 76.1 % 97.7 %, 74.1 % 44.1 %)' } } className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"></div>
+          </div>
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
               Announcing our next round of funding. <a href="#" className="font-semibold text-indigo-400"><span aria-hidden="true" className="absolute inset-0"></span>Read more <span aria-hidden="true">&rarr;</span></a>
