87421e4e7312e2cdd6513ddaa88ce640cce3e257
Serge07cloud <wisdomandy.5@gmail.com>
2025-11-09 23:09:18 +0000

    Install react icon library

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -22,9 +22,13 @@ const Welcome = () =>
           <div className="text-center">
             <h1 className="text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">I can help you turn your ideas in seamless digital experiences.</h1>
             <p className="mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">Hi, Welcome on my digital portfolio. I am <span className="text-xl  text-gray-50">Landry Konan</span>, a fullstack web developer building smart, fun and scalable web applications.</p>
+            <div className="border border-gray-50">
+              <p>Core technologies</p>
+              <div></div>
+            </div>
             <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a href="#" className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Get started</a>
-              <a href="#" className="text-sm/6 font-semibold text-white">Learn more <span aria-hidden="true">→</span></a>
+              <a href="#" className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Get in touch</a>
+              <a href="#" className="text-sm/6 font-semibold text-white">See projects <span aria-hidden="true">→</span></a>
             </div>
           </div>
         </div>
